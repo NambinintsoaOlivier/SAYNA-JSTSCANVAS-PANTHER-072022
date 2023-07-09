862ef5a48216ce0ddf882aae2580eb1b0603ee3f
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2023-07-09 13:04:50 +0300

    apparition de tous les elements en fade in de hgauche vers droite

---
+++ script.js
@@ -0,0 +1,27 @@
+// les variables et constantes
+var vide = "";
+const elements = document.querySelectorAll('.element');
+const elements1 = document.querySelectorAll('.element1');
+
+
+// affichage après une scroll
+window.addEventListener('scroll', () => {
+    const triggerBottom = window.innerHeight * 0.8;
+    elements.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        if (elementTop < triggerBottom) {
+            element.classList.add('show');
+        }
+    })
+})
+
+// animation d'entrée du header
+window.addEventListener('load', () => {
+    const triggerBottom = window.innerHeight * 0.8;
+    elements1.forEach(element1 => {
+        const elementTop = element1.getBoundingClientRect().top;
+        if (elementTop < triggerBottom) {
+            element1.classList.add('show');
+        }
+    })
+})
