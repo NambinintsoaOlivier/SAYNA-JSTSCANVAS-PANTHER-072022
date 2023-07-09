3a43c075228bb1028b22df1cdf8de3d7680da2bd
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2023-07-09 13:13:05 +0300

    defilement des image des personnages

--- script.js
+++ script.js
@@ -43,3 +43,64 @@ window.addEventListener('scroll', function() {
     var barre = document.getElementById('barre');
     barre.style.position = 'fixed';
 })
+
+// scrolling de barre des photos
+window.addEventListener('load', function() {
+    var conteneur = document.querySelector('.barre');
+    var suivant = document.getElementById('suivant');
+    var killmonger = document.querySelector(".Killmonger");
+    var okoye = document.querySelector(".okoye");
+    var shuri = document.querySelector(".shuri");
+    var nakia = document.querySelector(".nakia");
+    var ramonda = document.querySelector(".ramonda");
+    var kabi = document.querySelector(".kabi");
+    var baku = document.querySelector(".baku");
+    var nbclick = 0;
+
+    suivant.addEventListener('click', function() {
+        conteneur.scrollBy({
+            left: 551,
+            behavior: 'smooth',
+        });
+        nbclick++;
+        switch (nbclick) {
+            case 1:
+                killmonger.classList.add('contenu-principal');
+                break;
+            case 2:
+                okoye.classList.add('contenu-principal');
+                break;
+            case 3:
+                shuri.classList.add('contenu-principal');
+                break;
+            case 4:
+                nakia.classList.add('contenu-principal');
+                break;
+            case 5:
+                ramonda.classList.add('contenu-principal');
+                break;
+            case 6:
+                kabi.classList.add('contenu-principal');
+                break;
+            case 7:
+                baku.classList.add('contenu-principal');
+                break;
+            case 8:
+                break;
+            default:
+                killmonger.classList.remove('contenu-principal');
+                okoye.classList.remove('contenu-principal');
+                shuri.classList.remove('contenu-principal');
+                nakia.classList.remove('contenu-principal');
+                ramonda.classList.remove('contenu-principal');
+                kabi.classList.remove('contenu-principal');
+                kabi.classList.remove('contenu-principal');
+                baku.classList.remove('contenu-principal');
+                nbclick = 0;
+                conteneur.scrollBy({
+                    left: -5000,
+                    behavior: 'smooth',
+                });
+        }
+    });
+});
